654aa312dc4c7f72662bb496d378616dc09b340b
lisovski49 <145495467+lisovski49@users.noreply.github.com>
2023-11-20 15:57:02 +0300

    сделал 6

---
+++ lab6/1.py
@@ -0,0 +1,131 @@
+import sys
+import sqlite3
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+
+class QuizWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Квиз")
+        self.setGeometry(100, 100, 800, 600)
+        self.set_background_image("sea.jpg.webp")
+        self.set_stylesheet()
+
+        self.question_label = QLabel(self)
+        self.question_label.setGeometry(100, 100, 600, 200)
+        self.question_label.setAlignment(Qt.AlignCenter)
+        self.question_label.setStyleSheet("font-weight: bold; color: #FF0000;")
+
+        self.true_button = QPushButton("True", self)
+        self.true_button.setGeometry(200, 400, 150, 100)
+        self.true_button.clicked.connect(lambda: self.check_answer(True))
+
+        self.false_button = QPushButton("False", self)
+        self.false_button.setGeometry(450, 400, 150, 100)
+        self.false_button.clicked.connect(lambda: self.check_answer(False))
+
+        self.current_question = 0
+        self.correct_answers = 0  # Счетчик правильных ответов
+        self.questions = self.load_questions_from_database()
+
+        if len(self.questions) == 0:
+            self.create_database()
+            self.insert_questions_to_database()
+            self.questions = self.load_questions_from_database()
+
+        self.show_question()
+
+    def set_background_image(self, image_path):
+        background_label = QLabel(self)
+        background_pixmap = QPixmap(image_path)
+        background_label.setPixmap(background_pixmap.scaled(self.size(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation))
+        background_label.lower()
+
+    def set_stylesheet(self):
+        self.setStyleSheet("""
+            QWidget {
+                font-size: 24px;  
+            }
+
+            QPushButton {
+                height: 80px;
+                background-color: #4CAF50;
+                color: blue;
+                font-size: 24px;  
+            }
+
+            QLabel {
+                margin-bottom: 40px;  
+            }
+        """)
+
+    def create_database(self):
+        try:
+            connection = sqlite3.connect("quiz.db")
+            cursor = connection.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS questions (question TEXT, answer TEXT)")
+            connection.commit()
+            connection.close()
+        except sqlite3.Error as error:
+            print("Ошибка при создании базы данных:", error)
+
+    def load_questions_from_database(self):
+        try:
+            connection = sqlite3.connect("quiz.db")
+            cursor = connection.cursor()
+            cursor.execute("SELECT question, answer FROM questions")
+            questions = cursor.fetchall()
+            connection.close()
+            return questions
+        except sqlite3.Error as error:
+            print("Ошибка при загрузке вопросов из базы данных:", error)
+            return []
+
+    def insert_questions_to_database(self):
+        try:
+            connection = sqlite3.connect("quiz.db")
+            cursor = connection.cursor()
+
+            questions = [
+                ("Франция чемпион мира по футболу 2018", "True"),
+                ("Футбольное поле квадратное?", "False"),
+                ("Хоккей зимний вид спорта?", "True"),
+                ("Футбольный мяч круглый?", "True"),
+                ("В футбольной команде 10 игроков?", "False")
+            ]
+
+            cursor.executemany("INSERT INTO questions (question, answer) VALUES (?, ?)", questions)
+            connection.commit()
+            connection.close()
+        except sqlite3.Error as error:
+            print("Ошибка при добавлении вопросов в базу данных:", error)
+
+    def show_question(self):
+        if self.current_question < len(self.questions):
+            question, _ = self.questions[self.current_question]
+            self.question_label.setText(question)
+        else:
+            # Все вопросы отвечены
+            self.question_label.setText("Вы ответили правильно на {} вопрос(а/ов)".format(self.correct_answers))
+
+    def check_answer(self, user_answer):
+        _, correct_answer = self.questions[self.current_question]
+
+        if user_answer == (correct_answer == "True"):
+            QMessageBox.information(self, "Правильно!", "Вы правильно ответили!")
+            self.correct_answers += 1
+        else:
+            QMessageBox.information(self, "Неправильно!", "Вы неправильно ответили!")
+
+        self.current_question += 1
+        self.show_question()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = QuizWindow()
+    window.show()
+    sys.exit(app.exec_())
